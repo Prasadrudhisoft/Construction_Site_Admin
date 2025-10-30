921563ff1a9c4d21cd6180d02ff87685607e300d
samkankrale <shubham@rudhisoft.com>
2025-10-30 13:02:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -515,4 +515,4 @@ def test_db():
         return f"<h2>Database Error:</h2><p>{str(e)}</p>"
 
 if __name__ == "__main__":
-    app.run(debug=config.DEBUG, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
